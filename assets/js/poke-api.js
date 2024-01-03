7745b81ec38f9007819ea3d7f57b9323f19c3e5b
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2024-01-02 22:57:43 -0300

    Create new instance Pokemon poke-api.js
    Adiciona a criação de uma nova instância para pokemon.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,19 +1,37 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type1] = types
+
+
+    pokemon.types = types
+    pokemon.type = type1
+
+    pokemon.photo =pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+// Realiza requisições de detalhes
+pokeApi.getPokemons = async (offset=0, limit=5) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
-    return fetch(url)
-         .then((response) => response.json())
-         .then((jsonBody) => jsonBody.results)
-         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-         .then((detailRequests) => Promise.all(detailRequests))
-         .then((pokemonsDetails) => pokemonsDetails)
+    const response = await fetch(url)
+    const jsonBody = await response.json()
+    const pokemons = jsonBody.results
+    const detailRequests = pokemons.map(pokeApi.getPokemonDetail)
+    const pokemonsDetails = await Promise.all(detailRequests)
+    return pokemonsDetails
 }
-
-
